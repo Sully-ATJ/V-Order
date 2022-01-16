7e07ef7f2df2601bfeb5baab5fa45d6dadf2e0ee
Suleiman A. T Suleiman <suleiman3000@icloud.com>
2022-01-16 15:14:30 +0200

    added very basic speech recogniton interface

---
+++ js/speechtotext.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    var speechRecognition = window.webkitSpeechRecognition;
+
+    var recognition = new speechRecognition();
+
+    var textbox = $("#textbox");
+    var instructions = $("#instructions");
+
+    var content = '';
+
+    recognition.continuous = true;
+
+    //when speech is detected
+
+    recognition.onstart = function(){
+        instructions.text("Voice Recognition is on");
+    }
+
+    recognition.onspeechend = function(){
+        instructions.text("No Activity");
+    }
+
+    recognition.onerror = function(){
+        instructions.text("Try Again");
+    }
+
+    recognition.onresult = function(event){
+        var current  = event.resultIndex;
+
+        var transcript = event.results[current][0].transcript;
+
+        content += transcript;
+        textbox.val(content);
+    }
+
+    $("#start-btn").click(function(event){
+        if(content.length){
+            content += '';
+        }
+
+        recognition.start();
+    });
+
+}); 
